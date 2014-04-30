00680c24a875313e5496d9ffada9e7fbe72e3e75
caspar.wrede <caspar.wrede@androidpit.de>
2014-04-30 19:27:29 +0200

    Upates

--- src/com/justsendapp/OpenSender.java
+++ src/com/justsendapp/OpenSender.java
@@ -2,8 +2,9 @@ package com.justsendapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class OpenSender extends Activity {
   /**
@@ -11,13 +12,32 @@ public class OpenSender extends Activity {
    */
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-    sendIntent.setClassName("com.google.android.gm","com.google.android.gm.ComposeActivityGmail");
-    sendIntent.setType("plain/text");
-    sendIntent.putExtra(Intent.EXTRA_TEXT, "---\nSent distraction free with\nwww.justsendapp.com\n");
-    startActivity(sendIntent);
-    finish();
-
+      super.onCreate(savedInstanceState);
+      Intent sendIntent = new Intent(Intent.ACTION_SEND);
+      // Check whether gmail is installed
+      if ( appInstalled("com.google.android.gm")) {
+          sendIntent.setClassName("com.google.android.gm", "com.google.android.gm.ComposeActivityGmail");
+          sendIntent.putExtra(Intent.EXTRA_TEXT, "\n\n---\nSent distraction free with\nwww.justsendapp.com\n");
+          startActivity(sendIntent);
+      } else {
+          Toast.makeText(this, "This app requires gmail to be installed", Toast.LENGTH_SHORT).show();
+      }
+      finish();
   }
+    private boolean appInstalled(String uri)
+    {
+        PackageManager pm = getPackageManager();
+        boolean app_installed = false;
+        try
+        {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            app_installed = false;
+        }
+        return app_installed ;
+    }
+
 }
